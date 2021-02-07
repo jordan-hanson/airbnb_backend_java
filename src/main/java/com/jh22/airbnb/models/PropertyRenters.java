8d4a91b3b6483f5f05f458e6c8355baf869444f7
Jordan Hanson <JORDANMERCEDES22@GMAIL.COM>
2021-02-07 12:50:27 -0700

    many relationship propertyrenters created

--- src/main/java/com/jh22/airbnb/models/PropertyRenters.java
+++ src/main/java/com/jh22/airbnb/models/PropertyRenters.java
@@ -1,4 +1,68 @@
 package com.jh22.airbnb.models;
 
-public class PropertyRenters {
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "propertyrenters")
+public class PropertyRenters extends Auditable implements Serializable {
+
+    @Id
+    @ManyToOne
+    @NotNull
+    @JoinColumn(name = "userid")
+    @JsonIgnoreProperties(value = "properties",
+    allowSetters = true)
+    private User renters;
+
+    @Id
+    @ManyToOne
+    @NotNull
+    @JoinColumn(name = "propertyid")
+    @JsonIgnoreProperties(value = "users",
+    allowSetters = true)
+    private Property properties;
+
+    public PropertyRenters() {
+    }
+
+    public PropertyRenters(@NotNull User renters,
+                           @NotNull Property properties) {
+        this.renters = renters;
+        this.properties = properties;
+    }
+
+    public User getRenters() {
+        return renters;
+    }
+
+    public void setRenters(User renters) {
+        this.renters = renters;
+    }
+
+    public Property getProperties() {
+        return properties;
+    }
+
+    public void setProperties(Property properties) {
+        this.properties = properties;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PropertyRenters that = (PropertyRenters) o;
+        return getRenters().equals(that.getRenters()) && getProperties().equals(that.getProperties());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getRenters(), getProperties());
+    }
 }
