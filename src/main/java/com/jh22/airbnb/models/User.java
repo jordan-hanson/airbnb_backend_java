687ddc888d499a12a076ac9434e4ad941898f959
Jordan Hanson <JORDANMERCEDES22@GMAIL.COM>
2021-02-07 09:36:01 -0700

    user model created

---
+++ src/main/java/com/jh22/airbnb/models/User.java
@@ -0,0 +1,94 @@
+package com.jh22.airbnb.models;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "users")
+public class User extends Auditable{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long userid;
+
+    @NotNull
+    private String firstName;
+
+    @NotNull
+    private String lastName;
+
+    @NotNull
+    @Column(unique = true)
+    private String username;
+
+    @NotNull
+    @Column(unique = true)
+    @Email(message = "must be a valid email")
+    private String primaryemail;
+
+    @NotNull
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String password;
+
+    public User() {
+    }
+
+    public User(@NotNull String firstName, @NotNull String lastName, @NotNull String username, @NotNull @Email(message = "must be a valid email") String primaryemail, @NotNull String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.primaryemail = primaryemail;
+        this.password = password;
+    }
+
+    public long getUserid() {
+        return userid;
+    }
+
+    public void setUserid(long userid) {
+        this.userid = userid;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPrimaryemail() {
+        return primaryemail;
+    }
+
+    public void setPrimaryemail(String primaryemail) {
+        this.primaryemail = primaryemail;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
