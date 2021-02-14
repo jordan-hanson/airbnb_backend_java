ababc8879a5de1d80da6ab2770c73f03d2f84265
Jordan Hanson <JORDANMERCEDES22@GMAIL.COM>
2021-02-14 11:16:33 -0700

    user roles properties data sql created

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,28 @@
 DELETE
 FROM users;
+
+DELETE
+FROM roles;
+
+DELETE
+FROM properties;
+
+DELETE
+FROM cards;
+
+INSERT INTO users(userid, firstName, lastName, username, primaryemail, password, created_by, created_date, last_modified_by, last_modified_date)
+    VALUES (1, 'TestUser', 'Utest', 'testuser', 'testuser@email.com', 'testpassword', 'SYSTEM', CURRENT_TIMESTAMP, 'SYSTEM', CURRENT_TIMESTAMP),
+           (2, 'TestRenter', 'Rtest', 'testrenter', 'testrenter@email.com', 'testpassword', 'SYSTEM', CURRENT_TIMESTAMP, 'SYSTEM', CURRENT_TIMESTAMP),
+           (3, 'TestOwner', 'Otest', 'testowner', 'testowner@email.com', 'testpassword', 'SYSTEM', CURRENT_TIMESTAMP, 'SYSTEM', CURRENT_TIMESTAMP);
+
+INSERT INTO roles(roleid, name, created_by, created_date, last_modified_by, last_modified_date)
+    VALUES(1, 'ADMIN', 'SYSTEM', CURRENT_TIMESTAMP, 'SYSTEM', CURRENT_TIMESTAMP),
+          (1, 'USER', 'SYSTEM', CURRENT_TIMESTAMP, 'SYSTEM', CURRENT_TIMESTAMP),
+          (1, 'RENTER', 'SYSTEM', CURRENT_TIMESTAMP, 'SYSTEM', CURRENT_TIMESTAMP),
+          (1, 'OWNER', 'SYSTEM', CURRENT_TIMESTAMP, 'SYSTEM', CURRENT_TIMESTAMP);
+
+INSERT INTO properties(propertyid, title, description, street, city, state, zipcode, price, pictures, created_by, created_date, last_modified_by, last_modified_date)
+    VALUES(1, 'Cabin Getaway', 'Cozy Cottage in the high mountains.', '100 East Maughan Road', 'Lava Hot Springs', 'Idaho', 83246, 69.00, 'pictures', 'SYSTEM', CURRENT_TIMESTAMP, 'SYSTEM', CURRENT_TIMESTAMP),
+          (2, 'Couples Beach Resort', 'Romantic getaway on white sand beaches.', '300 S HoneySuckle Way', 'Liberty', 'Texas', 92475, 180.00, 'pictures', 'SYSTEM', CURRENT_TIMESTAMP, 'SYSTEM', CURRENT_TIMESTAMP),
+          (3, 'House Boat Family Spot', 'Fun for the whole family, includes jet skies.', '430 BullFrog Way', 'Paige', 'Arizona', 72341, 75.00, 'pictures', 'SYSTEM', CURRENT_TIMESTAMP, 'SYSTEM', CURRENT_TIMESTAMP),
+          (4, 'Desert Camper Getaway', 'Rustic camper in desert area.', '50 South Hobo Camp', 'IguanaWay', 'Nevada', 32467, 99.00, 'pictures', 'SYSTEM', CURRENT_TIMESTAMP, 'SYSTEM', CURRENT_TIMESTAMP);
