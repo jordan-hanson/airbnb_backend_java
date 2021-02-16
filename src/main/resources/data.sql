08cb3d4060a0c2b453b998a9b4da2e145203ec4e
Jordan Hanson <JORDANMERCEDES22@GMAIL.COM>
2021-02-15 22:33:18 -0700

    finished seed data joined tables

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -17,9 +17,9 @@ INSERT INTO users(userid, firstName, lastName, username, primaryemail, password,
 
 INSERT INTO roles(roleid, name, created_by, created_date, last_modified_by, last_modified_date)
     VALUES(1, 'ADMIN', 'SYSTEM', CURRENT_TIMESTAMP, 'SYSTEM', CURRENT_TIMESTAMP),
-          (1, 'USER', 'SYSTEM', CURRENT_TIMESTAMP, 'SYSTEM', CURRENT_TIMESTAMP),
-          (1, 'RENTER', 'SYSTEM', CURRENT_TIMESTAMP, 'SYSTEM', CURRENT_TIMESTAMP),
-          (1, 'OWNER', 'SYSTEM', CURRENT_TIMESTAMP, 'SYSTEM', CURRENT_TIMESTAMP);
+          (2, 'USER', 'SYSTEM', CURRENT_TIMESTAMP, 'SYSTEM', CURRENT_TIMESTAMP),
+          (3, 'RENTER', 'SYSTEM', CURRENT_TIMESTAMP, 'SYSTEM', CURRENT_TIMESTAMP),
+          (4, 'OWNER', 'SYSTEM', CURRENT_TIMESTAMP, 'SYSTEM', CURRENT_TIMESTAMP);
 
 INSERT INTO properties(propertyid, title, description, street, city, state, zipcode, price, pictures, created_by, created_date, last_modified_by, last_modified_date)
     VALUES(1, 'Cabin Getaway', 'Cozy Cottage in the high mountains.', '100 East Maughan Road', 'Lava Hot Springs', 'Idaho', 83246, 69.00, 'pictures', 'SYSTEM', CURRENT_TIMESTAMP, 'SYSTEM', CURRENT_TIMESTAMP),
@@ -33,4 +33,15 @@ INSERT INTO cards(cardid, name, number, type, expDate, securityCode, created_by,
           (3, 'Otest TestOwner', 3456789012, 'MASTERCARD', 4321, 432, 'SYSTEM', CURRENT_TIMESTAMP, 'SYSTEM', CURRENT_TIMESTAMP);
 
 INSERT INTO propertyowners(owner, property, subStDate, subExpDate, created_by, created_date, last_modified_by, last_modified_date)
-    VALUES(1, 1, CURRENT_DATE, CURRENT_DATE, 'SYSTEM', CURRENT_TIMESTAMP, 'SYSTEM', CURRENT_TIMESTAMP)
+    VALUES(1, 1, CURRENT_DATE, CURRENT_DATE, 'SYSTEM', CURRENT_TIMESTAMP, 'SYSTEM', CURRENT_TIMESTAMP),
+          (3, 2, CURRENT_DATE, CURRENT_DATE, 'SYSTEM', CURRENT_TIMESTAMP, 'SYSTEM', CURRENT_TIMESTAMP);
+
+INSERT INTO propertyrenters(renter, property, created_by, created_date, last_modified_by, last_modified_date)
+    VALUES(2, 4, 'SYSTEM', CURRENT_TIMESTAMP, 'SYSTEM', CURRENT_TIMESTAMP),
+          (2, 3, 'SYSTEM', CURRENT_TIMESTAMP, 'SYSTEM', CURRENT_TIMESTAMP);
+
+INSERT INTO userroles(user, role, created_by, created_date, last_modified_by, last_modified_date)
+    VALUES(1, 1, 'SYSTEM', CURRENT_TIMESTAMP, 'SYSTEM', CURRENT_TIMESTAMP),
+          (3, 4, 'SYSTEM', CURRENT_TIMESTAMP, 'SYSTEM', CURRENT_TIMESTAMP),
+          (2, 3, 'SYSTEM', CURRENT_TIMESTAMP, 'SYSTEM', CURRENT_TIMESTAMP),
+          (1, 2, 'SYSTEM', CURRENT_TIMESTAMP, 'SYSTEM', CURRENT_TIMESTAMP);
