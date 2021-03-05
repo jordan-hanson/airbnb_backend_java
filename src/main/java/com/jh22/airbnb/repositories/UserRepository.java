b7489660d4e621123e1e37f6913c5d5592a6ccd4
Jordan Hanson <JORDANMERCEDES22@GMAIL.COM>
2021-03-05 13:26:21 -0700

    test commit

--- src/main/java/com/jh22/airbnb/repositories/UserRepository.java
+++ src/main/java/com/jh22/airbnb/repositories/UserRepository.java
@@ -6,3 +6,5 @@ import org.springframework.data.repository.CrudRepository;
 public interface UserRepository extends CrudRepository<User, Long> {
     User findByUsername(String userName);
 }
+
+//Test
