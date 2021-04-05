b1d87efce6cb7bca85165a99efc373b8cb11fc55
Jordan Hanson <JORDANMERCEDES22@GMAIL.COM>
2021-04-05 05:25:32 -0600

    added user token access by role

--- src/main/java/com/jh22/airbnb/models/User.java
+++ src/main/java/com/jh22/airbnb/models/User.java
@@ -1,13 +1,17 @@
 package com.jh22.airbnb.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Entity
@@ -159,5 +163,19 @@ public class User extends Auditable{
 
     public void setCards(Set<CardInfo> cards) {
         this.cards = cards;
+    }
+    @JsonIgnore
+    public List<SimpleGrantedAuthority> getAuthority()
+    {
+        List<SimpleGrantedAuthority> rtnList = new ArrayList<>();
+
+        for (UserRoles r : this.roles)
+        {
+            String myRole = "ROLE" + r.getRole()
+                                        .getName()
+                                        .toUpperCase();
+            rtnList.add(new SimpleGrantedAuthority(myRole));
+        }
+        return rtnList;
     }
 }
