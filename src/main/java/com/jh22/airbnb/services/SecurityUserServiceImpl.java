d38672f63c81508f9cdfa451921dd2589350214a
Jordan Hanson <JORDANMERCEDES22@GMAIL.COM>
2021-04-06 15:46:12 -0600

    added transactional with secuirty userservice config file

--- src/main/java/com/jh22/airbnb/services/SecurityUserServiceImpl.java
+++ src/main/java/com/jh22/airbnb/services/SecurityUserServiceImpl.java
@@ -7,13 +7,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Transactional
 @Service(value = "securityUserService")
 public class SecurityUserServiceImpl implements UserDetailsService {
     @Autowired
     private UserRepository userRepo;
 
     @Override
+    @Transactional
     public UserDetails loadUserByUsername(String s) throws ResourceNotFoundException {
         User user = userRepo.findByUsername(s);
         if (user == null) {
