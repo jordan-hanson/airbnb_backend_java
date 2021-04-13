b6356865c70713a53d97b368f16347bc707bc519
Jordan Hanson <JORDANMERCEDES22@GMAIL.COM>
2021-04-12 22:57:01 -0600

    started seed data file

---
+++ src/main/java/com/jh22/airbnb/SeedData.java
@@ -0,0 +1,19 @@
+package com.jh22.airbnb;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.boot.CommandLineRunner;
+
+@Transactional
+@ConditionalOnProperty(
+        prefix = "command.line.runner",
+        value = "enabled",
+        havingValue = "true",
+        matchIfMissing = true
+)
+@Component
+public class SeedData
+        implements CommandLineRunner{
+    
+}
